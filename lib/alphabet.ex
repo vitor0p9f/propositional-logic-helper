e065f623a84ddcb33c6bf3c6fac426814ef7a1b0
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-08-02 21:30:22 +0000

    feat(Alphabet): create an Alphabet module
    Add some module attributes to store the symbols used in propositional
    logic.

---
+++ lib/alphabet.ex
@@ -0,0 +1,13 @@
+defmodule Alphabet do
+  @moduledoc """
+  A module to represents the propositional logic alphabet
+  """
+
+  @symbols ~r/[A-Z]|[01]|[\(\)]|[!&|>=]/
+  @propositional_symbols ~r/[A-Z]/
+  @truthy_symbols ~r/01/
+  @punctuation ~r/()/
+  @connectives ~r/!&|>=/
+  @connectives_precedence [!: 4, &: 3, |: 2, >: 1, =: 0]
+
+end
