ae987eface347a585e37c06e11d32edcdfec1cfa
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-08-02 21:40:12 +0000

    fix(Alphabet): add an escape character to the punctuation symbols

--- lib/alphabet.ex
+++ lib/alphabet.ex
@@ -6,7 +6,7 @@ defmodule Alphabet do
   @symbols ~r/[A-Z]|[01]|[\(\)]|[!&|>=]/
   @propositional_symbols ~r/[A-Z]/
   @truthy_symbols ~r/01/
-  @punctuation ~r/()/
+  @punctuation ~r/\(\)/
   @connectives ~r/!&|>=/
   @connectives_precedence [!: 4, &: 3, |: 2, >: 1, =: 0]
 
