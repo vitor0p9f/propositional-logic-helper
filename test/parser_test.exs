9339af5f70cd705490f61416c72757eb58f04b57
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-08-06 18:10:08 +0000

    test(Parser): refactor the test
    Refactor the test that checks if the 'SyntaxError' is raised when two or
    more propositonal symbols, truthy symbols, or connectives, except
    negation, are in sequence.

--- test/parser_test.exs
+++ test/parser_test.exs
@@ -3,8 +3,12 @@ defmodule ParserTest do
   doctest Parser
 
   describe "'SyntaxError' is raised" do
-    test "when two or more propositional symbols are in sequence" do
-      assert_raise SyntaxError, fn -> Parser.verify(["A", "A", "&", "B"]) end
+    test "when two or more propositional symbols, truthy symbols, or connectives, except negation, are in sequence" do
+      cases = [["A", "A", "&", "B"], ["!", "0", "1"], ["A", "&", "=", "B"]]
+
+      Enum.each(cases, fn case ->
+        assert_raise SyntaxError, fn -> Parser.verify(case) end
+      end)
     end
   end
 end
