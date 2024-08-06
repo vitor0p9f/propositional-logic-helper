03c482058d75b2c44c6faea3c4efc90b39f3862c
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-08-06 16:34:52 +0000

    test(Alphabet): refactor the connective precedence test

--- test/alphabet_test.exs
+++ test/alphabet_test.exs
@@ -2,7 +2,12 @@ defmodule AlphabetTest do
   use ExUnit.Case
   doctest Alphabet
 
-  test "get connective precedece value" do
-    assert Alphabet.connective_precedence(:|) == 2
+  test "connective precedece function" do
+    answers = [{:!, 4}, {:&, 3}, {:|, 2}, {:>, 1}, {:=, 0}]
+
+    Enum.each(answers, fn {connective, precedence} ->
+      assert Alphabet.connective_precedence(connective) == precedence
+    end)
+  end
   end
 end
