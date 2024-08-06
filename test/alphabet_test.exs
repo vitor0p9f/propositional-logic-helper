7521fef7a5ebc5842f777d317ae0ee2c64ec5052
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-08-06 16:37:41 +0000

    test(Alphabet): add a test to the symbol_type function
    Rename the test for the connective_precedence function.

--- test/alphabet_test.exs
+++ test/alphabet_test.exs
@@ -2,12 +2,30 @@ defmodule AlphabetTest do
   use ExUnit.Case
   doctest Alphabet
 
-  test "connective precedece function" do
+  test "the connective_precedece function" do
     answers = [{:!, 4}, {:&, 3}, {:|, 2}, {:>, 1}, {:=, 0}]
 
     Enum.each(answers, fn {connective, precedence} ->
       assert Alphabet.connective_precedence(connective) == precedence
     end)
   end
+
+  test "the symbol_type function" do
+    answers = [
+      {"A", :propositional},
+      {"0", :truthy},
+      {"1", :truthy},
+      {"(", :open},
+      {")", :close},
+      {"!", :negation},
+      {"&", :conjunction},
+      {"|", :disjunction},
+      {">", :implication},
+      {"=", :biconditional}
+    ]
+
+    Enum.each(answers, fn {symbol, type} ->
+      assert Alphabet.symbol_type(symbol) == type
+    end)
   end
 end
