cf4c8e78898722122025b56bf7b3679e22d9f1de
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-08-06 16:59:13 +0000

    test(Parser): refactor the messages of describe and test

--- test/parser_test.exs
+++ test/parser_test.exs
@@ -2,8 +2,8 @@ defmodule ParserTest do
   use ExUnit.Case
   doctest Parser
 
-  describe "wrong syntax" do
-    test "two or more propositional symbols in sequence" do
+  describe "'SyntaxError' is raised" do
+    test "when two or more propositional symbols are in sequence" do
       assert_raise SyntaxError, fn -> Parser.verify(["A", "A", "&", "B"]) end
     end
   end
