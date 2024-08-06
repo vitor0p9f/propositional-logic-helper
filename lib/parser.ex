81c5b41f5b5dd35fd2193221328777b8f69b4e27
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-08-06 17:24:42 +0000

    feat(Parser): add the 'verify' function
    Create the @next_allowed_symbols method attribute.
    
    Add spec and documentation to the 'verify' function.
    
    Create the 'check_symbol' private function.

--- lib/parser.ex
+++ lib/parser.ex
@@ -2,4 +2,67 @@ defmodule Parser do
   @moduledoc """
   Syntax verification of propositional logic.
   """
+
+  @next_allowed_symbols %{
+    propositional_and_truthy: [:close, :conjunction, :disjunction, :implication, :biconditional],
+    close: [:close, :conjunction, :disjunction, :implication, :biconditional],
+    open: [:propositional, :truthy, :open, :negation],
+    negation: [:propositional, :truthy, :open, :negation],
+    other_connectives: [:propositional, :truthy, :open, :negation]
+  }
+
+  @doc """
+  Verify the syntax of an expression belonging to propositional logic.
+
+  Returns `:ok` when the syntax is right.
+
+  Raise `SyntaxError` when encountering a syntax error.
+
+  ## Example
+
+      iex> Parser.verify([])
+      :ok
+
+      iex> Parser.verify(["!","!","A"])
+      :ok
+
+      iex> Parser.verify(["!","&","A"])
+      ** (SyntaxError) the '&' symbol cannot be after the '!' symbol.
+  """
+  @spec verify(list(String.t())) :: :ok
+  def verify([]), do: :ok
+
+  def verify([_]), do: :ok
+
+  def verify(list = [head, second | tail]) when is_list(list) do
+    allowed_symbols =
+      cond do
+        String.match?(head, Alphabet.propositional_symbols()) or
+            String.match?(head, Alphabet.truthy_symbols()) ->
+          @next_allowed_symbols[:propositional_and_truthy]
+
+        String.match?(head, Alphabet.punctuation()) and head == "(" ->
+          @next_allowed_symbols[:open]
+
+        String.match?(head, Alphabet.punctuation()) and head == ")" ->
+          @next_allowed_symbols[:close]
+
+        String.match?(head, Alphabet.connectives()) and head == "!" ->
+          @next_allowed_symbols[:negation]
+
+        String.match?(head, Alphabet.connectives()) and head != "!" ->
+          @next_allowed_symbols[:other_connectives]
+      end
+
+    check_symbol(allowed_symbols, head, second)
+
+    verify([second | tail])
+  end
+
+  defp check_symbol(allowed_symbols, previous_symbol, symbol) do
+    unless Enum.member?(allowed_symbols, Alphabet.symbol_type(symbol)) do
+      raise SyntaxError,
+        message: "the '#{symbol}' symbol cannot be after the '#{previous_symbol}' symbol."
+    end
+  end
 end
