128888be31894f357b05fd8ef10eb19859ba4167
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-08-07 20:33:52 +0000

    test(Alphabet): refactor the 'connective_precedence' function test
    Change the precedence of operators to allow the propositional logic to
    be written without using the negation symbol inside a parenthesis.

--- test/alphabet_test.exs
+++ test/alphabet_test.exs
@@ -3,7 +3,7 @@ defmodule AlphabetTest do
   doctest Alphabet
 
   test "the connective_precedece function" do
-    answers = [{:!, 4}, {:&, 3}, {:|, 2}, {:>, 1}, {:=, 0}]
+    answers = [{:&, 4}, {:|, 3}, {:>, 2}, {:=, 1}, {:!, 0}]
 
     Enum.each(answers, fn {connective, precedence} ->
       assert Alphabet.connective_precedence(connective) == precedence
