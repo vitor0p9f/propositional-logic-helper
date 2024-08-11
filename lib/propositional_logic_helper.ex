b2304d3826b284d69b0579567e6aaf5339417034
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-08-11 01:15:04 +0000

    feat(PropositionalLogicHelper): add the start function

--- lib/propositional_logic_helper.ex
+++ lib/propositional_logic_helper.ex
@@ -3,16 +3,40 @@ defmodule PropositionalLogicHelper do
   Documentation for `PropositionalLogicHelper`.
   """
 
-  @doc """
-  Hello world.
+  @greetings ~s"
+Welcome to the Propositional Logic Helper!
 
-  ## Examples
+Use the symbols below to write the logical expression.
 
-      iex> PropositionalLogicHelper.hello()
-      :world
+Propositional symbols: Character between A and Z, all uppercase.
+0: false
+1: true
+Negation: !
 
-  """
-  def hello do
-    :world
+Use parenthesis to define the precedence, if parenthesis não estiverem presentes, a ordem de avaliação dos operadores será, do maior para o menor:
+
+!: 4
+&: 3
+|: 2
+>: 1
+=: 0
+"
+
+  use Application
+
+  def start() do
+    IO.puts(@greetings)
+
+    expression = String.trim(IO.gets("Type the logical expression: "), "\n")
+
+    IO.puts("")
+
+    Lexer.analyze(String.split(expression, "", trim: true))
+
+    Parser.verify(String.split(expression, "", trim: true))
+
+    rpn_expression = Parser.generate_RPN(String.split(expression, "", trim: true))
+
+    TruthTable.generate(expression, rpn_expression)
   end
 end
